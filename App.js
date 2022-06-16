30929a5333d0c5028c184006fa02a67d0fdb051e
chennakesiBalaraju <87874918+chennakesiBalaraju@users.noreply.github.com>
2022-06-16 10:24:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Components} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import green from './components/green';
+function App() extends Components {
+  render(){
+  return (
+    <div className="App">
+      <green />
+    </div>
+  );
+}
+}
+export default App;
